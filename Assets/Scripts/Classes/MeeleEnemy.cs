a3ab7ea4cde1022d97c23c32b428eb553baf8905
Jan España <104784400+JoaquinAlcazar@users.noreply.github.com>
2024-11-26 18:19:15 +0100

    Commit rapido
    medio hecho el coso del enemigo mele

--- Assets/Scripts/Classes/MeeleEnemy.cs
+++ Assets/Scripts/Classes/MeeleEnemy.cs
@@ -5,16 +5,30 @@ using UnityEngine;
 public class MeeleEnemy : AEnemy
 {
     private float explosionRange;
+    public float speed;
+    private float initialSpeed;
+    private Transform playerPos;
+    private Ray ray;
+    private RaycastHit hit;
+    public float rayDistance;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerPos = GameObject.FindWithTag("Player").transform;
+        initialSpeed = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, playerPos.position, Time.deltaTime * speed);
+        ray = new Ray(transform.position, playerPos.position);
+        if (Physics.Raycast(ray, out hit, rayDistance))
+        {
+            if (hit.transform.tag == "Player") speed = 0;
+            else speed = initialSpeed;
+        }
+
     }
 
     void Explode()
