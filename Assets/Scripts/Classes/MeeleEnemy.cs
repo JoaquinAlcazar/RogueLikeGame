311d69bab57a96a517806946582f2703fe682df2
Jan España <104784400+JoaquinAlcazar@users.noreply.github.com>
2024-11-27 20:43:33 +0100

    Raycast batalla
    Que se pelee el joaquin del futuro con el puto raycast

--- Assets/Scripts/Classes/MeeleEnemy.cs
+++ Assets/Scripts/Classes/MeeleEnemy.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class MeeleEnemy : AEnemy
 {
-    private float explosionRange;
+    public float explosionRange;
     public float speed;
     private float initialSpeed;
     private Transform playerPos;
     private Ray ray;
     private RaycastHit hit;
-    public float rayDistance;
+    private CircleCollider2D explosionCollider;
+
+    private LineRenderer lineRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
         playerPos = GameObject.FindWithTag("Player").transform;
         initialSpeed = speed;
+        explosionCollider = GetComponent<CircleCollider2D>();
+        explosionCollider.radius = explosionRange;
+        explosionCollider.enabled = false;             
     }
 
     // Update is called once per frame
@@ -23,16 +29,30 @@ public class MeeleEnemy : AEnemy
     {
         transform.position = Vector3.MoveTowards(transform.position, playerPos.position, Time.deltaTime * speed);
         ray = new Ray(transform.position, playerPos.position);
-        if (Physics.Raycast(ray, out hit, rayDistance))
+
+        if (Physics.Raycast(ray, out hit, 100))
         {
-            if (hit.transform.tag == "Player") speed = 0;
-            else speed = initialSpeed;
+            Debug.Log("Hit");
+            if (hit.transform.tag == "Player")
+            {
+                speed = 0;
+                StartCoroutine(Explode());
+            }
         }
+    }
+
+    IEnumerator Explode()
+    {
+        new WaitForSeconds(2);
+        explosionCollider.enabled = true;
+        yield return StartCoroutine(explosionAnimation());
+        Destroy(gameObject);
 
     }
 
-    void Explode()
+    IEnumerator explosionAnimation()
     {
-
+        //reproducir animacíón de explosión
+        return null;
     }
 }
