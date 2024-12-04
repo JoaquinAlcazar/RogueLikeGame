35b5089c2f31a0c561cec46a67a2fbbd2614b79e
Jan España <104784400+JoaquinAlcazar@users.noreply.github.com>
2024-12-04 19:48:31 +0100

    Intento patético de granada
    null reference o algo así. Que venga Dios a ayudarme.

--- Assets/Scripts/Classes/Grenade.cs
+++ Assets/Scripts/Classes/Grenade.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class Grenade : AProjectile
 {
+    public float explosionRange;
+    public CircleCollider2D circleCollider;
+    private Animator animator;
+    public AnimationClip clip;
+    private SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        circleCollider = GetComponent<CircleCollider2D>();
+        circleCollider.isTrigger = true;
+        circleCollider.enabled = false;
+        new StartCoroutine(Explode());
     }
 
     // Update is called once per frame
@@ -16,8 +26,16 @@ public class Grenade : AProjectile
         
     }
 
-    void Explode()
+    IEnumerator Explode()
     {
+        new WaitForSeconds(3);
+        spriteRenderer.enabled = false;
+        circleCollider.enabled = false;
+        return null;
+    }
 
+    void PlayExplosionClip()
+    {
+        animator.Play("GrenadeExplosion");
     }
 }
