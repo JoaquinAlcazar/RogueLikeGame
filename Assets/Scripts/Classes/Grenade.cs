ca9d27267af0a24dfbe929bc0616e14e163a1fae
Jan España <104784400+JoaquinAlcazar@users.noreply.github.com>
2024-12-11 19:35:32 +0100

    Granada casi terminada
    Dios ha acontecido ahora solo falta que la explosión sea efectiva. Otros 129864 años de development

--- Assets/Scripts/Classes/Grenade.cs
+++ Assets/Scripts/Classes/Grenade.cs
@@ -6,39 +6,54 @@ public class Grenade : AProjectile
 {
     public float explosionRange;
     public CircleCollider2D circleCollider;
-    private Animation animator;
+    public Animator animator;
     public AnimationClip clip;
     private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rb;
+    public float launchForce;
+
+    private Vector2 launchDirection;
     // Start is called before the first frame update
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        animator = GetComponent<Animation>();
+        animator = GetComponent<Animator>();
         circleCollider = GetComponent<CircleCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
         circleCollider.isTrigger = true;
         circleCollider.enabled = false;
-        animator.AddClip(clip, clip.name);
+
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0f; // Asegúrate de que Z sea 0 para 2D
+        launchDirection = (mousePosition - transform.position).normalized;
+
         StartCoroutine(Explode());
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (rb.velocity.magnitude < 0.1f && launchDirection != Vector2.zero)
+        {
+            rb.AddForce(launchDirection * launchForce, ForceMode2D.Impulse);
+            launchDirection = Vector2.zero; // Evitar que la fuerza se aplique nuevamente
+        }
     }
 
     IEnumerator Explode()
     {
         yield return new WaitForSeconds(3);
-        spriteRenderer.enabled = false;
+        spriteRenderer.sprite = null;
         circleCollider.enabled = false;
         PlayExplosionClip();
-        yield return new WaitForSeconds(clip.length);
+        yield return new WaitForSeconds(0.6f);
         Destroy(gameObject);
     }
 
     void PlayExplosionClip()
     {
-        animator.Play(clip.name);
+        animator.SetTrigger("Explode");
     }
+
 }
